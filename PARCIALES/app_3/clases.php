3583cfb64b7e2144dd776a5324bfd7f41678110b
Olorenzoj <olorenzo2004@gmail.com>
2024-10-14 00:11:22 -0500

    TALLER_7 - Paso 2: Implementación básica de sesiones

--- PARCIALES/app_3/clases.php
+++ PARCIALES/app_3/clases.php
@@ -1,30 +1,113 @@
 <?php
 
-interface Detalle {
+interface Detalle
+{
     public function obtenerDetallesEspecificos(): string;
 }
 
-abstract class Entrada implements Detalle {
+{
+
+}
+
+abstract class Entrada implements Detalle
+{
     public $id;
     public $fecha_creacion;
     public $tipo;
     public $titulo;
     public $descripcion;
 
-    public function __construct($datos = []) {
+    public function __construct($datos = [])
+    {
         foreach ($datos as $key => $value) {
             if (property_exists($this, $key)) {
                 $this->$key = $value;
             }
         }
     }
+
     abstract public function obtenerDetallesEspecificos(): string;
 }
 
-class GestorBlog {
+class EntradUnaColumna extends Entrada
+{
+    public $titulo;
+    public $descripcion;
+
+    public function __construct($id, $fecha_creacion, $titulo, $descripcion)
+    {
+        parent::__construct($id, $fecha_creacion, 1);
+        $this->titulo = $titulo;
+        $this->descripcion = $descripcion;
+    }
+
+    // Implementación del método obtenerDetallesEspecificos
+    public function obtenerDetallesEspecificos(): string
+    {
+        return "Entrada de una columna: " . $this->titulo;
+    }
+}
+
+class EntradaDosColumnas extends Entrada
+{
+    public $titulo1;
+    public $descripcion1;
+    public $titulo2;
+    public $descripcion2;
+
+    public function __construct($id, $fecha_creacion, $titulo1, $descripcion1, $titulo2, $descripcion2)
+    {
+        parent::__construct($id, $fecha_creacion, 2);
+        $this->titulo1 = $titulo1;
+        $this->descripcion1 = $descripcion1;
+        $this->titulo2 = $titulo2;
+        $this->descripcion2 = $descripcion2;
+    }
+
+    // Implementación del método obtenerDetallesEspecificos
+    public function obtenerDetallesEspecificos(): string
+    {
+        return "Entrada de dos columnas: " . $this->titulo1 . " | " . $this->titulo2;
+    }
+}
+
+class EntradaTresColumnas extends Entrada
+{
+    public $titulo1;
+    public $descripcion1;
+    public $titulo2;
+    public $descripcion2;
+    public $titulo3;
+    public $descripcion3;
+
+    public function __construct($id, $fecha_creacion, $titulo1, $descripcion1, $titulo2, $descripcion2, $titulo3, $descripcion3)
+    {
+        parent::__construct($id, $fecha_creacion, 3);
+        $this->titulo1 = $titulo1;
+        $this->descripcion1 = $descripcion1;
+        $this->titulo2 = $titulo2;
+        $this->descripcion2 = $descripcion2;
+        $this->titulo3 = $titulo3;
+        $this->descripcion3 = $descripcion3;
+    }
+
+    // Implementación del método obtenerDetallesEspecificos
+    public function obtenerDetallesEspecificos(): string
+    {
+        return "Entrada de tres columnas: " . $this->titulo1 . " | " . $this->titulo2 . " | " . $this->titulo3;
+    }
+}
+
+{
+
+}
+
+class GestorBlog
+{
     private $entradas = [];
 
-    public function cargarEntradas() {
+    public function cargarEntradas()
+    {
         if (file_exists('blog.json')) {
             $json = file_get_contents('blog.json');
             $data = json_decode($json, true);
@@ -34,66 +117,16 @@ class GestorBlog {
         }
     }
 
-    public function guardarEntradas() {
-        $data = array_map(function($entrada) {
+    public function guardarEntradas()
+    {
+        $data = array_map(function ($entrada) {
             return get_object_vars($entrada);
         }, $this->entradas);
         file_put_contents('blog.json', json_encode($data, JSON_PRETTY_PRINT));
     }
 
-    public function obtenerEntradas() {
+    public function obtenerEntradas()
+    {
         return $this->entradas;
     }
-
-    public function agregarEntrada(Entrada $entrada){
-        $this-> entradas[] = $entrada;
-    }
-    public function eliminarEntrada(Entrada $entrada){
-        foreach ($this -> entradas as $key => $entrada) {
-            if($entrada-> id == $entrada){
-                unset($this -> entradas[$key]);
-                break;
-            }
-        }
-    }
-    
 }   
-
- class EntradaUnaColumna extends Entrada{
-    public $titulo;
-    public $descricion;
-
-  public function obtenerDetallesEspecificos(): string{
-    return "Entrada de una columna: $this->titulo";
-  }
-    
-
-}
-
-class EntradaDosColumnas extends Entrada {
-    public $titulo1;
-    public $descripcion1;
-    public $titulo2;
-    public $descripcion2;
-
-    public function obtenerDetallesEspecificos(): string{
-        return "Entrada de 2 columnas: $this->titulo1" + " $this->titulo2";
-      }
-
-}
-
-class EntradaTresColumnas extends Entrada{
- public $titulo1;
- public $descripcion1;
- public $titulo2;
- public $descripcion2;
- public $titulo3;
-
- public $descripcion3;
-
- public function obtenerDetallesEspecificos(): string{
-    return "Entrada de 2 columnas: $this->titulo1" + " $this->titulo2" + "$this->titulo3";
-  }
-
-}
-
